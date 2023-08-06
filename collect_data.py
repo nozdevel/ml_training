ae3623cf055f7a9c28c8e44673ca2bf42b28dada
nozdevel <noz.devel@gmail.com>
2023-08-07 00:18:46 +0900

    temprary2

--- collect_data.py
+++ collect_data.py
@@ -10,8 +10,7 @@ from typing import Tuple
 DATA_DIR = 'data'
 DATA_FILE = DATA_DIR + '/raw_data.csv'
 
-SHUTSUBA_URL = ''
-RESULT_URL = ''
+HORSE_TOTAL = 18
 
 def initialize():
     os.makedirs(DATA_DIR, exist_ok=True)
@@ -60,11 +59,30 @@ def parse_race_condition(race_condition: str) -> int:
 
     return int(ret)
 
+def parse_horse_sex_barei(horse_info: str) -> Tuple[int, int]:
+    tmp = horse_info
+
+    if '牝' in tmp:
+        sex = 0
+        tmp = tmp.replace('牝', '')
+    elif '牡' in tmp:
+        sex = 1
+        tmp = tmp.replace('牡', '')
+    elif 'セ' in tmp:
+        sex = 2
+        tmp = tmp.replace('セ', '')
+    else:
+        print(f'horse sex error: {tmp}')
+        sys.exit(1)
+
+    return int(sex), int(tmp)
+
 def main():
     initialize()
 
     race_id = str(year) + str(place).zfill(2) + str(kaisai).zfill(2) + \
               str(kaisai_day).zfill(2) + str(race_num).zfill(2)
+
 
     req_url = SHUTSUBA_URL + race_id
 
@@ -97,6 +115,42 @@ def main():
     print(parse_race_condition(race_condition_sub))
     race_condition = parse_race_condition(race_condition_sub)
 
+    print(len(uma_list))
+    shusso = len(uma_list)
+
+    tmp_horse_list = []
+
+    for i in range(HORSE_TOTAL):
+        waku = -1
+        umaban = -1
+        uma_id = -1
+        sex = -1
+        barei = -1
+        sekiryo = -1
+        kisyu_id = -1
+        bataiju = -1
+        zogen = -1
+
+        if i >= (shusso - 1):
+            tmp_horse_list.append([waku, umaban, uma_id, sex, barei, sekiryo, kisyu_id, bataiju, zogen])
+            continue
+
+        uma_info = uma_list[i]
+
+        waku = int(str(uma_info.find_all(class_='Txt_C')[0]).split('>')[2].split('<')[0])
+        umaban = int(str(uma_info.find_all(class_='Txt_C')[1]).split('>')[1].split('<')[0])
+        uma_id = int(str(uma_info.find(class_='HorseName').find('a')).split()[7].split('_')[1].replace('"', ''))
+        tmp = str(uma_info.find(class_='Barei')).split('>')[1].split('<')[0]
+        sex, barei = parse_horse_sex_barei(tmp)
+        sekiryo = float(str(uma_info.find_all(class_='Txt_C')[3]).split('>')[1].split('<')[0])
+        kisyu_id = int(str(uma_info.find(class_='Jockey').find('a')).split('"')[1].split('/')[6])
+        bataiju = int(str(uma_info.find(class_='Weight')).split('>')[1].split('<')[0].replace('\n', ''))
+        zogen = int(str(uma_info.find(class_='Weight')).split('(')[1].split(')')[0])
+
+        tmp_horse_list.append([waku, umaban, uma_id, sex, barei, sekiryo, kisyu_id, bataiju, zogen])
+
+    print(len(tmp_horse_list))
+    print(tmp_horse_list)
 
 if __name__ == '__main__':
     main()
